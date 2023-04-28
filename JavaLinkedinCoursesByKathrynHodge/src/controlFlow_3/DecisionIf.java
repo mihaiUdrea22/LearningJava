4eb462022deda6f29098cce022f1936242ed9604
mihaiUdrea22 <araripeit@gmail.com>
2023-04-28 16:46:18 +0300

    First commit form this course

---
+++ JavaLinkedinCoursesByKathrynHodge/src/controlFlow_3/DecisionIf.java
@@ -0,0 +1,16 @@
+package controlFlow_3;
+
+import java.util.Scanner;
+
+public class DecisionIf {
+
+    public static void main(String[] args) {
+        System.out.println("Pick a number between 1 and 10");
+        Scanner scanner = new Scanner(System.in);
+        int inputtedNum = scanner.nextInt();
+        System.out.println(inputtedNum);
+
+
+    }
+
+}
